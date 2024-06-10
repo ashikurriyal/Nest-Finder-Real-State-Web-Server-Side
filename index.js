d723084e9eee24dc7cc9ac212d16df9142469565
ashikurriyal <riyal.ashikur@gmail.com>
2024-06-10 20:27:29 +0600

    Updated the user authentication get and post

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 // const cookieParser = require('cookie-parser');
 const app = express()
-require ('dotenv').config()
+require('dotenv').config()
 const port = process.env.PORT || 5300;
 
 
@@ -10,6 +10,52 @@ app.use(express.json())
 app.use(cors());
 
 
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ozyesnk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        //await client.connect();
+        // Send a ping to confirm a successful connection
+        //await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+
+        //Collections
+        const usersCollection = client.db('NestFinderDB').collection('users')
+
+
+        //users
+        app.get('/users', async (req, res) => {
+            const result = await usersCollection.find().toArray()
+            res.send(result);
+        })
+
+        app.post('/users', async (req, res) => {
+            const assignment = req.body;
+            const result = await usersCollection.insertOne(assignment)
+            res.send(result);
+        })
+
+
+
+
+    } finally {
+        // Ensures that the client will close when you finish/error
+        //await client.close();
+    }
+}
+run().catch(console.dir);
 app.get('/', (req, res) => {
     res.send('Nest Finder Server is OnGoing!')
 })
